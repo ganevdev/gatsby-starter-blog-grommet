9a79bb68541b053a52a8b3eac2c2082359feb1f9
Ivan Ganev <iganevru@gmail.com>
2018-09-17 03:02:02 +0300

    ini

---
+++ gatsby-config.js
@@ -0,0 +1,67 @@
+const siteConfig = require('./site-config');
+
+module.exports = {
+  siteMetadata: {
+    ...siteConfig,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        //trackingId: `ADD YOUR TRACKING ID HERE`,
+      },
+    },
+    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Gatsby Starter Blog`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/assets/gatsby-icon.png`,
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: 'gatsby-plugin-typography',
+      options: {
+        pathToConfigModule: 'src/utils/typography',
+      },
+    },
+    `gatsby-plugin-styled-components`,
+  ],
+}
---
+++ site-config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  siteTitle: `gatsby-starter-blog-grommet-v2`,
+  siteTitleShort: `BlogGrommet`,
+  siteDescription: `An opinionated starter for Gatsby.`,
+  siteUrl: `/`,
+  // themeColor: `#000`,
+  // backgroundColor: `#fff`,
+  pathPrefix: null,
+  // logo: path.resolve(__dirname, 'src/images/icon.png'),
+  // social: {
+  //   twitter: `gatsbyjs`,
+  //   fbAppId: `966242223397117`,
+  // },
+};
