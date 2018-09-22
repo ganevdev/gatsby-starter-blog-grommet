60b901d5116a2ebcab3f4d8a0541d11282cec669
Ivan Ganev <iganevru@gmail.com>
2018-09-22 21:28:21 +0300

    some config

--- site-config.js
+++ site-config.js
@@ -1,19 +1,22 @@
 module.exports = {
+  //
+  // This file is for easy and fast website changes. Change the information to your own.
+  //
   // Site Info:
   title: 'Starter Gatsby Blog',
   description: 'This is a DEMO site! SSG - Gatsby JS, UI - Grommet v2',
   siteUrl: 'https://ganevru.github.io/gatsby-starter-blog-grommet/',
-  footerText: '**2018**', // You can use Markdown markup here
+  footerText: '**2018**<br><br>You can use Markdown markup here', // You can use Markdown markup here
 
-  // Services:
-  googleAnalyticsTrackingId: `ADD YOUR TRACKING ID HERE`,
-
-  // Author Info:
+  // Blog Author Info:
   author: 'Ivan Ganev',
   authorImage:
     'https://pbs.twimg.com/profile_images/986603715732934656/7A4Nt68k_400x400.jpg',
   authorBio:
-    "Hello, I'm Ivan Ganev and I made this blog starter for Gatsby js using UI Grommet v2. <br><br>Repo of this starter: [github.com/ganevru/gatsby-starter-blog-grommet](https://github.com/ganevru/gatsby-starter-blog-grommet)", // You can use Markdown markup here
+    "Hello, I'm Ivan Ganev and I made this blog starter for Gatsby js using UI Grommet v2. <br><br>Repo of this starter: [github.com/ganevru/gatsby-starter-blog-grommet](https://github.com/ganevru/gatsby-starter-blog-grommet) <br><br>Replace this text with your own: site-config.js ", // You can use Markdown markup here
+
+  // Services:
+  googleAnalyticsTrackingId: `ADD YOUR TRACKING ID HERE`,
 
   // Site Style:
   font: 'Roboto', // Roboto
