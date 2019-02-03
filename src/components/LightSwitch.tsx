8276822aafb820eed88c0528474e04a2fca7143d
Ivan Ganev <iganevru@gmail.com>
2019-02-03 17:59:33 +0300

    Button Light Switch

--- src/components/LightSwitch.tsx
+++ src/components/LightSwitch.tsx
@@ -1,8 +1,34 @@
 import React from 'react';
-import { Button } from 'grommet';
+import { Button, Box, Text } from 'grommet';
+import { Actions } from 'grommet-icons';
 
 export default (props) => (
-  <Button margin={'medium'} onClick={props.onClick} primary>
-    {props.themeType}
-  </Button>
+  <Box margin="small">
+    <Button onClick={props.onClick} plain>
+      <Box
+        direction="row-responsive"
+        pad={{ vertical: 'small', horizontal: 'medium' }}
+        round="small"
+        background={{ color: 'neutral-2', opacity: 'weak' }}
+        border={{
+          color: 'border',
+          size: 'xsmall'
+        }}
+        align="center"
+        elevation="small"
+      >
+        <Actions size="medium" />
+        <Text
+          margin={{ horizontal: '6px' }}
+          color="text"
+          size="large"
+          weight="bold"
+        >
+          {props.themeType === 'dark'
+            ? 'Switch to a Light'
+            : 'Switch to a Dark'}
+        </Text>
+      </Box>
+    </Button>
+  </Box>
 );
